57c6a82e84652c40760cf029eecb36994347ee7d
sposs <steph.poss@gmail.com>
2019-09-09 22:52:20 +0200

    Add admin part

--- collect/admin.py
+++ collect/admin.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from collect.models import Measurement
+from collect.models import Measurement, Sensor
 
 
 class MeasureAdmin(admin.ModelAdmin):
     list_display = ("date", "value")
 
 
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("name", "rpi_type", "rpi_port")
+
+
 admin.site.register(Measurement, MeasureAdmin)
+admin.site.register(Sensor, SensorAdmin)
 
