0708368dba96cceacf57a81907f03aa7e5e5f56f
sposs <stephposs@gmail.com>
2019-09-29 21:59:10 +0200

    Work on command support

---
+++ collect/management/commands/get_commands.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+import email
+import imaplib
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        conn = imaplib.IMAP4_SSL(host=settings.EMAIL_IMAP)
+        conn.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+        conn.select()
+        typ, data = conn.search(None, "SUBJECT", 'Command')
+        if typ != "OK":
+            conn.logout()
+            raise CommandError("Cannot read emails")
+        mail_ids = data[0].split()
+        for email_id in mail_ids:
+            typ, data = conn.fetch(email_id, '(RFC822)')
+            raw_email = data[0][1]
+            raw_email_string = raw_email.decode('utf-8')
+            email_message = email.message_from_string(raw_email_string)
+            parts = email_message.get_payload()
+            if not isinstance(parts, list):
+                parts = [parts]
+            message = None
+            for part in parts:
+                message = part.get_payload()
+                break
+            print(message)
+        conn.logout()
